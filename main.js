0064821eac8a7472c6addff1d6062912fb819624
Michael Jackson <miglja619@gmail.com>
2025-01-09 16:28:25 -0500

    Refactor to use dynamic HTML

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 // run browserify main.js -o bundle.js after updating
 
 let Phrase = require("mgj-palindrome");
-let string = prompt("Please enter a string for palindrome testing:");
 
-let phrase = new Phrase(string);
-if (phrase.palindrome()) {
-    alert(`"${phrase.content} is a palindrome!`);
-} else {
-    alert(`"${phrase.content} is not a palindrome!`)
+function palindromeTester(event) {
+    event.preventDefault();
+    let phrase = new Phrase(event.target.phrase.value);
+    let palindromeResult = 
+        document.querySelector("#palindromeResult");
+    if (phrase.palindrome()) {
+        palindromeResult.innerHTML = `<strong>"${phrase.content}"</strong> is a palindrome!`;
+    } else {
+        palindromeResult.innerHTML = `<strong>"${phrase.content}"</strong> is not a palindrome!`;
+    }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    let tester = document.querySelector("#palindromeTester");
+    tester.addEventListener("submit", function(event) {
+        palindromeTester(event);
+    });
+});
